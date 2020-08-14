2776a005633ead012dd2da75a39c066f4385e65e
joseph Evangelista <oootep123@gmail.com>
2020-08-14 00:03:25 -0400

    implemented hamburger menu

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-document.querySelector
+const navSlide = () => {
+  const burger = document.querySelector(".burger")
+  const nav = document.querySelector(".main-nav")
+
+  burger.addEventListener("click", () => {
+    nav.classList.add("nav-active")
+    burger.style.display = "none"
+  })
+}
+
+const closeNav = () => {
+  const close = document.querySelector(".close-burger")
+  const burger = document.querySelector(".burger")
+  const nav = document.querySelector(".main-nav")
+
+  close.addEventListener("click", () => {
+    nav.classList.remove("nav-active")
+    burger.style.display = "inline"
+  })
+}
+
+navSlide()
+closeNav()
